c6ea339044b976c96565527720d96dd730b28624
ettdro <ett.drolet@hotmail.com>
2016-09-20 20:50:27 -0400

    Importation de l'application.

---
+++ ConnectTheDotsProjet/app/src/main/java/com/example/etienne/connectthedots/GameActivity.java
@@ -0,0 +1,30 @@
+package com.example.etienne.connectthedots;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class GameActivity extends View {
+
+    private Paint paint;
+
+    public GameActivity(Context context) {
+        super(context);
+
+        // create the Paint and set its color
+        paint = new Paint();
+        paint.setColor(Color.GRAY);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        canvas.drawColor(Color.BLUE);
+        canvas.drawCircle(200, 200, 100, paint);
+    }
+
+}
---
+++ ConnectTheDotsProjet/app/src/main/java/com/example/etienne/connectthedots/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.etienne.connectthedots;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void exitAppOnClick(View view) {
+        finish();
+    }
+
+    public void buttonOnClick(View view) {
+        Intent intent = new Intent(this, GameActivity.class);
+        startActivity(intent);
+    }
+}
